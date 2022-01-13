bc2c8a717b577fdcfa0916fd2d62f24db7f856ff
keemzeehae <keemzeehae@naver.com>
2022-01-13 16:34:03 +0900

    contact commit

---
+++ src/com/jihye/s1/contact/FriendDTO.java
@@ -0,0 +1,12 @@
+package com.jihye.s1.contact;
+
+public class FriendDTO {
+	
+	
+	private String name;
+	private String memo;
+	private String email;
+	private int number;
+	private int birth;
+
+}
