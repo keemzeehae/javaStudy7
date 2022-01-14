f1ec6f287544bfd317d2296e0e99bb899e9dbde9
keemzeehae <keemzeehae@naver.com>
2022-01-14 11:48:09 +0900

    hashmap

--- src/com/jihye/s1/contact/FriendMain.java
+++ src/com/jihye/s1/contact/FriendMain.java
@@ -1,10 +1,21 @@
 package com.jihye.s1.contact;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class FriendMain {
 
 	public static void main(String[] args) {
 		FriendController fc = new FriendController();
 		fc.choose();
+		
+		ArrayList<Integer> ar = new ArrayList<>();
+		ArrayList<Integer> ar2 = new ArrayList<>();
+		
+		HashMap<String, ArrayList<Integer>> map = new HashMap<>();
+		map.put("number1", ar);
+		map.put("number2", ar2);
+		
 
 	}
 
